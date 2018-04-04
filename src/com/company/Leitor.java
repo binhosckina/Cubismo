79ebabe469a86f35363e6aaf2714f38eadbe6875
Gabriel Correa <binhosckina@gmail.com>
2018-04-03 22:41:29 -0300

    Primeiro refactor

--- src/com/company/Leitor.java
+++ src/com/company/Leitor.java
@@ -9,22 +9,32 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Leitor {
+    public Leitor() {
+        leitura("teste000100");
+    }
 
     private void leitura(String origem) {
         Path caminho = Paths.get(origem);
-        try (Scanner scannerLinha = new Scanner(Files.newBufferedReader(caminho, Charset.defaultCharset()))) {
-            Scanner scannerPalavra = new Scanner(Files.newBufferedReader(caminho, Charset.defaultCharset()));
-            scannerLinha.useDelimiter("[\n]");
-            scannerPalavra.useDelimiter(" ");
+        try (Scanner scanner = new Scanner(Files.newBufferedReader(caminho, Charset.defaultCharset()))) {
 
             String linhaAtual = "";
-            String palavraAtual;
+            int linhas = scanner.nextInt();
 
-            while (scannerLinha.hasNextLine()) {
-                
+            while (scanner.hasNext()) {
+                int x1 = scanner.nextInt();
+                int y1 = scanner.nextInt();
+                int x2 = scanner.nextInt();
+                int y2 = scanner.nextInt();
+                linhaAtual = scanner.next();
+
+                desenha(x1, y1, x2, y2, linhaAtual);
             }
         } catch (IOException e) {
             System.err.format("Erro de E/S: %s%n", e);
         }
+    }
+
+    private void desenha(int x1, int y1, int x2, int y2, String linha) {
+
     }
 }
