caf51a84a02e03024216b38ae8eeeb9ced1340a0
Gabriel Correa <binhosckina@gmail.com>
2018-04-04 22:30:24 -0300

    Alterações nas classes

--- src/com/company/Leitor.java
+++ src/com/company/Leitor.java
@@ -6,9 +6,14 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Leitor {
+
+    List<Retangulo> lista = new ArrayList<>();
+
     public Leitor() {
         leitura("teste000100");
     }
@@ -17,7 +22,7 @@ public class Leitor {
         Path caminho = Paths.get(origem);
         try (Scanner scanner = new Scanner(Files.newBufferedReader(caminho, Charset.defaultCharset()))) {
 
-            String linhaAtual = "";
+            String cor = "";
             int linhas = scanner.nextInt();
 
             while (scanner.hasNext()) {
@@ -25,16 +30,21 @@ public class Leitor {
                 int y1 = scanner.nextInt();
                 int x2 = scanner.nextInt();
                 int y2 = scanner.nextInt();
-                linhaAtual = scanner.next();
+                cor = scanner.next();
 
-                desenha(x1, y1, x2, y2, linhaAtual);
+                desenha(x1, y1, x2, y2, cor);
             }
         } catch (IOException e) {
             System.err.format("Erro de E/S: %s%n", e);
         }
     }
 
-    private void desenha(int x1, int y1, int x2, int y2, String linha) {
+    private void desenha(int x1, int y1, int x2, int y2, String cor) {
+        Retangulo r = new Retangulo(x1, y1, x2, y2, cor);
+        adiciona(r);
+    }
+
+    private void adiciona(Retangulo r) {
 
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Leitor leitor = new Leitor();
+
+        System.out.println(leitor);
     }
 }
